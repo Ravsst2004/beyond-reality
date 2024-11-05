5779bd440f3060354b2a64399fa2ce6fee4c43b3
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-11-05 21:50:42 +0800

    init

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import Menu from "./Menu";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="container max-w-6xl mx-auto px-6 md:px-0">
+        <Menu />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Menu.tsx
@@ -0,0 +1,7 @@
+const Menu = () => {
+  return <nav className="flex items-center justify-between font-bold">
+    <img src="images/logo.svg" alt="" />
+  </nav>;
+};
+
+export default Menu;
